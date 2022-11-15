3998f6222c0ca0f7d676c6b9a15dfda0325981ef
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2022-11-15 20:56:38 +0530

    Read bz2 File to extract titles

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import bz2
 filename="./DumpFiles/enwiki-latest-pages-articles1.xml-p1p41242.bz2"
+outputFileName="./WikiGraph/graph.txt"
+graphFile=open(outputFileName,'w',encoding='utf-8')
 bzFile = bz2.BZ2File(filename)
 c=0
-for nextline in bzFile:
-    pass
+while True:
+    nextLine=bzFile.readline()
+    if not nextLine:
+        break
+    line=str(nextLine, 'utf-8').strip()
+    if line=="<page>":
+        while line!="</page>":
+            if line.startswith('<title>'):
+                title= line[7:-8]
+            nextLine=bzFile.readline()
+            line=str(nextLine, 'utf-8').strip()
+        
+        graphFile.write(title+'\n')
+        c+=1
+print(c)
