0e7847cf27cc3059f10be9455ace118bdf39b0ab
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2022-11-18 00:26:18 +0530

    Outlinks and categories extracted

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import bz2
+import xml.etree.ElementTree as ET
+import re
 filename="./DumpFiles/enwiki-latest-pages-articles1.xml-p1p41242.bz2"
 outputFileName="./WikiGraph/graph.txt"
+
 graphFile=open(outputFileName,'w',encoding='utf-8')
 bzFile = bz2.BZ2File(filename)
 c=0
@@ -10,12 +13,28 @@ while True:
         break
     line=str(nextLine, 'utf-8').strip()
     if line=="<page>":
+        categories=[]
+        outEdges=[]
+        pageLines=[]
         while line!="</page>":
-            if line.startswith('<title>'):
-                title= line[7:-8]
-            nextLine=bzFile.readline()
-            line=str(nextLine, 'utf-8').strip()
-        
-        graphFile.write(title+'\n')
+            pageLines.append(line)
+            line=str(bzFile.readline(),'utf-8').strip()
+        pageLines.append(line)
+        pageRoot = ET.fromstring("\n".join(i for i in pageLines))
+        for child in pageRoot:
+            for nextchild in child:
+                if nextchild.tag=='text':
+                    listOfLinks=re.findall('\[\[([^\[\]]*)\]\]',nextchild.text)
+                    for link in listOfLinks:
+                        link=str(link.split('|')[0])
+                        if link.startswith('Category'):
+                            categories.append(link[9:])
+                        else:
+                            if ':' not in link:
+                                outEdges.append(link)
+        print(outEdges[:10])
+        print(categories[:10])
+        if c>1:
+            break
         c+=1
 print(c)
