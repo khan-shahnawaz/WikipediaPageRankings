ecdc697b63034490260cccbdaa49a06ebaa6bbd6
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2022-12-01 10:29:35 +0530

    Completed Random Walk

--- lib/randomWalk.py
+++ lib/randomWalk.py
@@ -5,31 +5,40 @@ import heapq
 class RandomWalk():
     def __init__(self, graph:WikiGraphCreator) -> None:
         self.helperFileName = graph.helperFileName
+        self.graphFileName = graph.graphFileName
         self.offsetMap = {}
         self.nodeList={}
         self.numNodes=0
         self.numVisits=[]
+        self.nodeIndex={}
     def loadGraph(self) -> None:
         with open(self.helperFileName, 'r', encoding='utf-8') as f:
             curLine = f.readline()
             while curLine:
                 nodeName = curLine.strip()
                 self.nodeList[self.numNodes]= nodeName
+                self.nodeIndex[nodeName]=self.numNodes
                 
                 curLine = f.readline()
                 categoryOffset, outlinkOffset = curLine.strip().split(' ')
                 self.offsetMap[self.numNodes] = (int(categoryOffset), int(outlinkOffset))
                 self.numNodes+=1
                 curLine = f.readline()
-    def randomWalk(self, startNode:str, numSteps:int) -> list:
-        path = [startNode]
-        for i in range(numSteps):
-            with open(self.helperFileName, 'r', encoding='utf-8') as f:
-                f.seek(self.offsetMap[startNode][1])
-                numOutLinks = int(f.readline().strip())
-                outLinks = []
-                for i in range(numOutLinks):
-                    outLinks.append(f.readline().strip())
-                startNode = random.choice(outLinks)
-                path.append(startNode)
-        return path
+        self.numVisits=[(0,i) for i in range(self.numNodes)]
+    def randomWalk(self, startNode: int, walkLength: int) -> None:
+        curNode = self.nodeIndex[startNode]
+        self.numVisits[curNode][0]+=1
+        
+        graphFile = open(self.graphFileName, 'r', encoding='utf-8')
+        for i in range(walkLength):
+            adjacent = []
+            categoryOffset, outlinkOffset = self.offsetMap[curNode]
+            graphFile.seek(outlinkOffset)
+            numNeighbors = int(graphFile.readline().strip())
+            for j in range(numNeighbors):
+                neighbor = graphFile.readline().strip()
+                if neighbor not in self.nodeIndex:
+                    continue
+                adjacent.append(self.nodeIndex[neighbor])
+            curNode=random.choice(adjacent)
+            self.numVisits[curNode][0]+=1
