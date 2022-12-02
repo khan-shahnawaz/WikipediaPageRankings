27c7afb389c0e8ea17ca948495c2e94d450defd5
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2022-12-02 16:50:04 +0530

    Added comments

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from lib.randomWalk import RandomWalk
 wikiCreate = WikiGraphCreator(dumpFileLocation='./DumpFiles/enwiki-latest-pages-articles.xml.bz2',
                              graphFileLocation= './WikiGraph/graph.txt')
 
-
+'''
 wikiCreate.createGraph()
 wikiCreate.completed=True
 wikiCreate.createHelperFile()
 wikiCreate.printStatistics()
-
+'''
 
 randomWalker = RandomWalk(wikiCreate)
 randomWalker.loadGraph()
